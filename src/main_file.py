56ff272f278a713d1b0bed47c66567bb9b21e395
bibao <chuyingbbd@163.com>
2018-03-11 21:53:08 +0800

    edit the error for downsample in theano missing.

--- src/main_file.py
+++ src/main_file.py
@@ -33,10 +33,13 @@ def main_02():
     print('training NN2 running....%d:%d:%d end' % (t1.tm_hour,t1.tm_min, t1.tm_sec))
 
 def main_03():
+    t1 = time.gmtime(time.time())
+    print('training NN3 running....%d:%d:%d' % (t1.tm_hour, t1.tm_min, t1.tm_sec))
     from network3 import Network
     from network3 import ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer
 
     training_data, validation_data, test_data = network3.load_data_shared()
+    mini_batch_size = 10
     net = network3.Network([
         ConvPoolLayer(image_shape = (mini_batch_size, 1, 28, 28),
                   filter_shape = (20, 1, 5, 5),
@@ -47,6 +50,8 @@ def main_03():
         FullyConnectedLayer(n_in=40*4*4, n_out=100),
         SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
     net.SGD(training_data, 60, mini_batch_size, 0.1, validation_data, test_data)
+    t1 = time.gmtime(time.time())
+    print('training NN3 running....%d:%d:%d end' % (t1.tm_hour,t1.tm_min, t1.tm_sec))
 
 if __name__ == "__main__":
     #main_01()
